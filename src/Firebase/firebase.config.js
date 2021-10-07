cb7fec06b7aca819c3f6075ef9f927c726bd6218
ImranRH321 <imranrh247@gmail.com>
2021-10-07 15:50:30 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyArHevfkIc32_RfkJJPwdDxeIVFF8r--bQ",
+    authDomain: "react-firebase-authentic-c3f7f.firebaseapp.com",
+    projectId: "react-firebase-authentic-c3f7f",
+    storageBucket: "react-firebase-authentic-c3f7f.appspot.com",
+    messagingSenderId: "716168235239",
+    appId: "1:716168235239:web:0b6acc0ae97540a5b2fa42"
+  };
+
+  export default firebaseConfig;
